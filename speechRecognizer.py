74e877dbb7a368ceed77e0b9f757cca8a0856f8f
mart rietdijk <mart.rietdijk@gmail.com>
2020-05-30 20:17:15 +0000

    added GPIO functionality

--- speechRecognizer.py
+++ speechRecognizer.py
@@ -1,6 +1,11 @@
 import speech_recognition as sr
 from playsound import playsound
+import RPi.GPIO as GPIO
+import os
 
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.OUT)
 r = sr.Recognizer()
 mic = sr.Microphone()
 
@@ -10,11 +15,15 @@ while 1:
              audio = r.listen(source)
         voice = r.recognize_google(audio)
         print(voice)
-        if("Alex" in voice):
-            if("what is my name" in voice):
-                print("Your name is Mart Rietdijk. The best person in this entire universe")
-            elif("lights off" in voice):
-                print("Lights shutting off")
-                playsound('./audio_files/lights_off.mp3')
+        
+        if("what is my name" in voice):
+            print("Your name is Mart Rietdijk. The best person in this entire universe")
+        elif("lights off" in voice):
+            print("Lights shutting off")
+            GPIO.output(26, GPIO.LOW)
+            playsound('./audio_files/lights_off.mp3')
+        elif("lights on" in voice):
+            print("Lights on")
+            GPIO.output(26, GPIO.HIGH)
     except:
         pass
